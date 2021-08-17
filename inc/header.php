4bcd2690e38d7356acf32d00824603537d8d5336
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-18 03:02:35 +0600

    Blog With PHP OOP (Blog Pagination) css

--- inc/header.php
+++ inc/header.php
@@ -8,7 +8,7 @@
 	<meta name="author" content="Delowar">
 	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
 	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/style.css">
 	<script src="js/jquery.js" type="text/javascript"></script>
 	<script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
 
