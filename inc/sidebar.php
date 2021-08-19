1b43e33ae655c1536776b70e83a8f78afcae126e
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-20 00:30:59 +0600

    Blog With PHP OOP (Showing latest Posts)

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -19,30 +19,20 @@
 			
 			<div class="samesidebar clear">
 				<h2>Latest articles</h2>
+				<?php
+			$query = "select * from tbl_post limit 5";
+			$post = $db->select($query);
+			if($post)
+			{
+				while($result = $post->fetch_assoc()){
+			?>
 					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+						<h3><a href="post.php?id=<?php echo $result['id'] ?>"><?php echo $result['title'] ?></a></h3>
+						<a href="post.php?id=<?php echo $result['id'] ?>"><img src="admin/uploads/<?php echo $result['image'] ?>" alt="post image"/></a>
+						<?php echo $fm->textShorten($result['body'],120) ?>	
 					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-	
+			
+					<?php } } else { header("Location:404.php"); } ?>
 			</div>
 			
 		</div>
