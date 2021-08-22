f884fe145a99f057f67cf65bab174c4f888ef262
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-22 23:13:58 +0600

    Blog With PHP OOP (Admin Logout Option)

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,3 +1,7 @@
+<?php 
+include "../lib/Session.php";
+Session::checkSession(); 
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -44,10 +48,15 @@
                 <div class="floatright">
                     <div class="floatleft">
                         <img src="img/img-profile.jpg" alt="Profile Pic" /></div>
+                        <?php
+                        if (isset($_GET['action']) && $_GET['action'] == "logout") {
+                           Session::destroy();
+                        }
+                        ?>
                     <div class="floatleft marginleft10">
                         <ul class="inline-ul floatleft">
                             <li>Hello Admin</li>
-                            <li><a href="#">Logout</a></li>
+                            <li><a href="?action=logout">Logout</a></li>
                         </ul>
                     </div>
                 </div>
