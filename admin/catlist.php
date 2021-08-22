03a507449ae0b6f85b11997f30cc8c68cef8254a
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-22 23:47:48 +0600

    Blog With PHP OOP (Showing Categories in Admin Panel)

--- admin/catlist.php
+++ admin/catlist.php
@@ -13,46 +13,20 @@
 						</tr>
 					</thead>
 					<tbody>
+						<?php 
+						$query = "select * from tbl_category order by id desc";
+						$category = $db->select($query);
+						if ($category) {
+							$i=0;
+							while ($result = $category->fetch_assoc()) {
+								$i++;
+						?>
 						<tr class="odd gradeX">
-							<td>01</td>
-							<td>Internet</td>
+							<td><?php echo $i; ?></td>
+							<td><?php echo $result['name'] ?></td>
 							<td><a href="">Edit</a> || <a href="">Delete</a></td>
 						</tr>
-						<tr class="even gradeC">
-							<td>02</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>03</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>04</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-							<tr class="odd gradeX">
-							<td>05</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>06</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>07</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>08</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
+						<?php }} ?>
 					</tbody>
 				</table>
                </div>
