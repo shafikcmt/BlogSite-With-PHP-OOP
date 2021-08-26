689005f90766df4e900099cc644a9ade73d1d984
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-27 05:23:23 +0600

    checkLogin Function create for not access after login admin panel

--- lib/Session.php
+++ lib/Session.php
@@ -25,6 +25,13 @@ class Session
 
       }
   }
+
+  public static function checkLogin(){
+    self::init();
+    if (self::get("login")==true) {
+    header("Location:index.php");
+    }
+}
   public static function destroy()
   {
       session_destroy();
