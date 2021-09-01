9c8e9da98a3c81a5c70c942af46ffd7633522d70
shafikcmt <mdshafiqulislam822@gmail.com>
2021-09-02 04:33:04 +0600

    Blog With PHP OOP (Delete Messages from Seen Box)

--- admin/inbox.php
+++ admin/inbox.php
@@ -64,6 +64,19 @@ if (isset($_GET['seenid'])) {
 
 			<div class="box round first grid">
                 <h2>Seen Message</h2>
+				<?php 
+				if (isset($_GET['delid'])) {
+					$delid = $_GET['delid'];
+					$delquery = "delete from tbl_contact where id='$delid'";
+					$delcontact = $db->delete($delquery);
+					if ($delcontact) {
+						echo "<span class='success'>Message Deleted successfully.</span>";
+						}else {
+							echo "<span class='error'>Message Not Deleted !</span>";
+	
+						}
+				}
+				?>
                 <div class="block">        
 				<table class="data display datatable" id="example">
 					<thead>
@@ -93,8 +106,8 @@ if (isset($_GET['seenid'])) {
 							<td><?php echo  $fm->textShorten($result['body'],30); ?></td>
 							<td><?php echo $fm->formatDate($result['date']); ?></td>
 							<td>
+								<a href="viewmsg.php?viewmsgid=<?php echo $result['id'] ?>">View</a> || 
 								<a onclick="return confirm('Are You sure to Delete !!')" href="?delid=<?php echo $result['id'] ?>">Delete</a>
-							
 							</td>
 						</tr>
 						<?php }}?>
