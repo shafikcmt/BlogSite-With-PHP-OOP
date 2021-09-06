f6ec13ea58be2695641c19028f118017233f9ba9
shafikcmt <mdshafiqulislam822@gmail.com>
2021-09-07 02:09:39 +0600

    Blog With PHP OOP (Display/Showing User List and delete)

---
+++ admin/userlist.php
@@ -0,0 +1,81 @@
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/sidebar.php'; ?>
+        <div class="grid_10">
+            <div class="box round first grid">
+                <h2>User List</h2>
+				<?php 
+				if (isset($_GET['deluser'])) {
+					$deluserid = $_GET['deluser'];
+					$delquery = "delete from tbl_user where id='$deluserid'";
+					$deluserdata = $db->delete($delquery);
+					if ($deluserdata) {
+						echo "<span class='success'>User Deleted successfully.</span>";
+						}else {
+							echo "<span class='error'>User Not Deleted !</span>";
+	
+						}
+				}
+				?>
+                <div class="block">        
+                    <table class="data display datatable" id="example">
+					<thead>
+						<tr>
+							<th>Serial No.</th>
+							<th>Name</th>
+							<th>Username</th>
+							<th>Email</th>
+							<th>Details</th>
+							<th>Role</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+						$query = "select * from tbl_user order by id desc";
+						$alluser = $db->select($query);
+						if ($alluser) {
+							$i=0;
+							while ($result = $alluser->fetch_assoc()) {
+								$i++;
+						?>
+						<tr class="odd gradeX">
+							<td><?php echo $i; ?></td>
+							<td><?php echo $result['name'] ?></td>
+							<td><?php echo $result['username'] ?></td>
+							<td><?php echo $result['email'] ?></td>
+							<td><?php echo $fm->textShorten($result['detail'],30) ?></td>
+							<td>
+                            <?php 
+                            if ($result['role']=="1") {
+                               echo "Admin";
+                            }elseif ($result['role']=="2") {
+                                echo "Author";
+                            }elseif ($result['role']=="3") {
+                                echo "Editor";
+                            }
+                            ?>
+                        </td>
+							<td><a href="viewuser.php?userid=<?php echo $result['id']; ?>">View</a> || <a onclick="return confirm('Are You sure to Delete !!')" href="?deluser=<?php echo $result['id']; ?>">Delete</a></td>
+						</tr>
+						<?php }} ?>
+					</tbody>
+				</table>
+               </div>
+            </div>
+        </div>
+<script type="text/javascript">
+
+$(document).ready(function () {
+setupLeftMenu();
+
+$('.datatable').dataTable();
+setSidebarHeight();
+
+
+});
+
+</script>
+
+<?php include 'inc/footer.php'; ?>
+
+
